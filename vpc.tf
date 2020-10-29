8f52fc1361d8f676e2eb699a02a470d419aa9a11
sushma-gurunule <70726241+sushma-gurunule@users.noreply.github.com>
2020-10-29 17:12:27 +0000

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -14,7 +14,7 @@ resource "aws_vpc" "sush-vpc" {
 resource "aws_subnet" "sush-public-1" {
   vpc_id                  = aws_vpc.sush-vpc.id
   cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = "true"
+  map_public_ip_on_launch = "false"
   availability_zone       = "us-east-1a"
 
   tags = {
@@ -25,7 +25,7 @@ resource "aws_subnet" "sush-public-1" {
 resource "aws_subnet" "sush-public-2" {
   vpc_id                  = aws_vpc.sush-vpc.id
   cidr_block              = "10.0.2.0/24"
-  map_public_ip_on_launch = "true"
+  map_public_ip_on_launch = "false"
   availability_zone       = "us-east-1b"
 
   tags = {
